10f403091db61dc3721d159021815c2d6c874028
RPMorrigan <morriganpriscilla@gmail.com>
2026-01-14 14:17:30 -0700

    checkpoint

--- version-1/src/components/CountryCard.jsx
+++ version-1/src/components/CountryCard.jsx
@@ -1,26 +1,39 @@
+import { Link } from "react-router-dom";
+
 function CountryCard({ count }) {
-    // TODO: Attempt destructuring.
-    const flag = count.flags.svg;
-    const name = count.name.common;
-    const population = count.population.toLocaleString();
-    const capital = count.capital;
-    const region = count.region;
+// I was manually loading each value independently, but I think this is a lot cleaner.
+    const {
+        flags: { svg: flag },
+        name: { common: name },
+        population,
+        capital,
+        region
+    } = count;
     
+    const countryName = useParams();
+    console.log(countryName);
+
     return (
-        // This card is comprised of a main div called 'card', an image for the flag, and a content div that holds the 
-        <div className="card">
-            <img src={flag} alt={name} />
-            <div className="card-content">
-                <h3>{name}</h3>
-                {/* Span is used here to style the labels. These are simmply bolded. */}
-                <p><span>Population:</span> {population}
-                    <br/>
-                    <span>Capital:</span> {capital}
-                    <br/>
-                <span>Region:</span> {region}</p>
-            </div>
-        </div>
-    )
+        <>
+            {/* Creates a link to a detailed page for each country. */}
+            <Link to={`/CountryDetail/${name}`} className="card-link">
+                {/* This card is comprised of a main div called 'card', an image for the flag, and a content div that holds the  */}
+                <div className="card">
+                    <img src={flag} alt={name} />
+                    <div className="card-content">
+                        <h3>{name}</h3>
+                        {/* Span is used here to style the labels. These are simmply bolded. */}
+                        <p><span>Population:</span> {population}
+                            <br/>
+                            <span>Capital:</span> {capital}
+                            <br/>
+                        <span>Region:</span> {region}
+                        </p>
+                    </div>
+                </div>
+            </Link>
+        </>
+    );
 }
 
 export default CountryCard;
--- version-1/src/pages/CountryDetail.jsx
+++ version-1/src/pages/CountryDetail.jsx
@@ -1,7 +1,19 @@
-function CountryDetail() {
+import { useParams } from "react-router-dom";
+
+function CountryDetail({ countries }) {
+    const {name} = useParams();
+    
     return (
-        1 + 1
+        <>
+            <h2>Country Detail Page</h2>
+            <p>{countryName.countryName}</p>
+        </>
     )
+}
+
+// Loader funciton
+export const countryDetailLoader = async ({ params }) => {
+    const countryName = params.countryName;
 }
 
 export default CountryDetail;
