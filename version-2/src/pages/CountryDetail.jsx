a39a5c18c1f4a49c99df4988126d4323aa6d442f
RPMorrigan <morriganpriscilla@gmail.com>
2026-02-05 23:39:22 -0700

    finished the country counter, but I still need to finish the save button and make sure that the saved countries are loading

--- version-2/src/pages/CountryDetail.jsx
+++ version-2/src/pages/CountryDetail.jsx
@@ -1,4 +1,5 @@
 import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const CountryDetail = ({ countries = [] }) => {
     // Stores just the name of the necessary country
@@ -7,9 +8,61 @@ const CountryDetail = ({ countries = [] }) => {
     const navigate = useNavigate();
     // This says, if there is a country object with the same name as the one we're looking for in countries, load it into our variable, 'country'.
     const country = countries?.find(count => count.name.common.toLowerCase() === countryName.toLowerCase());
+    // View count
+    const [viewCount, setViewCount] = useState(0);
+    
+    useEffect(() => {
+        if (!country) return;
+
+        const getViewCount = async () => {
+            try {
+                let response = await fetch(
+                    '/api/update-one-country-count',
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            "country_name": country.name.common
+                        })
+                    }
+                );
+                const data = await response.json();
+                setViewCount(data.count);
+            } catch (error) {
+                console.log('Error: ' + error.message);
+            }
+        }
         
+        getViewCount();
+    }, [country]);
+    
+    console.log(viewCount);
+
     if (!country) {
         return <div> Country not found!</div>
+    }
+
+    const getCountry = async () => {
+        try {
+            let response = await fetch(
+                '/api/save-one-country',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        "country_name": country.name.common
+                    })
+                }
+            );
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.log('Error: ' + error.message);
+        }
     }
 
     return (
@@ -19,8 +72,10 @@ const CountryDetail = ({ countries = [] }) => {
                 <img src={country.flags.svg} alt={country.name.common} />
             <div className="details">
             <h2>{ countryName }</h2>
-                    <button className="save-button">Save</button>
+                    <button className="save-button" onClick={getCountry}>Save</button>
                     <div className="details-paragraphs">
+                    {/* View count */}
+                        <p>View Count: {viewCount}</p>
                     {/* to local string formats the raw digits into something more legible */}
                     <p>Population: {country.population.toLocaleString()}</p>
                     {/* I see a lot of exception handling when I'm looking for solutions, so I am attempting to remember to add these things */}
