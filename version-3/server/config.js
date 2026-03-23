7a04548470f5763c700447df1b3f2f67a893819f
RPMorrigan <morriganpriscilla@gmail.com>
2026-03-23 12:07:16 -0600

    cleaned up

---
+++ version-3/server/config.js
@@ -0,0 +1,6 @@
+const config = {
+    databaseUrl:
+        "postgresql://neondb_owner:npg_kty7mZUx9LTO@ep-lively-truth-akjqnvkr-pooler.c-3.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+};
+
+export default config;
